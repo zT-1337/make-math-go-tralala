90f7242fe163fdf527b12ce6b9f32a682581b416
Thomas Zerr <thomas.zerr@gmail.com>
2026-01-03 20:24:01 +0100

    Change to using options module
    Started to use the options module to manipulate wav file properties.
    
    Refactor main function to make it easier to keep track which memmory
    needs to get freed in case of an error.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "../include/musical_notes.h"
+#include "../include/options.h"
 #include "../include/tralala.h"
 #include "../include/wav.h"
 #include <stdint.h>
@@ -8,15 +9,15 @@
 
 #define MATHERS_COUNT 5
 
-int main() {
-  srand(time(NULL));
+uint8_t generate_wav_files(Options *opts) {
   char filename_buffer[128];
   uint8_t (*mathers[MATHERS_COUNT])(uint8_t) = {linear, quadratic, sinus,
                                                 logarithmic, randomized};
+
   for (int i = 0; i < MATHERS_COUNT; ++i) {
     Wav *audio_data =
-        init_wav(PCM_AUDIO_FORMAT, STEREO_CHANNEL, CD_SAMPLE_RATE,
-                 DEFAULT_SAMPLE_SIZE_IN_BITS, AUDIO_LENGTH_IN_SECONDS);
+        init_wav(PCM_AUDIO_FORMAT, opts->channel_count, opts->sample_rate,
+                 opts->bits_per_sample, AUDIO_LENGTH_IN_SECONDS);
 
     math_to_tralala(audio_data, mathers[i]);
 
@@ -30,4 +31,14 @@ int main() {
   }
 
   return 0;
+}
+
+int main(int argc, char **argv) {
+  Options *opts = parse_options(argc, argv);
+
+  srand(time(NULL));
+  uint8_t result = generate_wav_files(opts);
+
+  free(opts);
+  return result;
 }
