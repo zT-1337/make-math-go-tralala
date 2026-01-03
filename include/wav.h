38a9d02db4871d6c8b0ce3d04b56a4deb37b43b1
Thomas Zerr <thomas.zerr@gmail.com>
2026-01-03 06:10:32 +0100

    Add wav functionality
    Add initialization of wav structure
    Add freeing of this structure
    Add persisting this structure into a valid wav file

---
+++ include/wav.h
@@ -0,0 +1,42 @@
+#include <stdint.h>
+
+#ifndef ZT_WAV_H
+#define ZT_WAV_H
+
+// Audio Format Definitions
+#define PCM_AUDIO_FORMAT 1
+
+// Channel Definitions
+#define MONO_CHANNEL 1
+#define STEREO_CHANNEL 2
+
+// Sample Rate Definitions
+#define CD_SAMPLE_RATE 44100
+
+// Sample Size Definitions
+#define DEFAULT_SAMPLE_SIZE_IN_BITS 16
+
+// Write to file results
+#define WRITE_WAV_SUCCESS 0
+#define WRITE_WAV_COULD_NOT_OPEN_ERROR 1
+#define WRITE_WAV_COULD_NOT_WRITE_HEADER 2
+#define WRITE_WAV_COULD_NOT_WRITE_DATA 3
+
+typedef struct Wav {
+  uint16_t audio_format;
+  uint16_t channel_count;
+  uint32_t sample_rate;
+  uint16_t bits_per_sample;
+  uint32_t sample_count;
+  int8_t *sample_bytes;
+} Wav;
+
+Wav *init_wav(uint16_t audio_format, uint16_t channel_count,
+              uint32_t sample_rate, uint16_t bits_per_sample,
+              uint32_t length_in_seconds);
+
+void free_wav(Wav *data);
+
+uint8_t write_wav_to_file(Wav *data, char *filename);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include "../include/wav.h"
+#include <stdint.h>
+
+#define AUDIO_FILENAME "sample.wav"
+#define AUDIO_LENGTH_IN_SECONDS 10
 
 int main() {
-  printf("hello world!\n");
-  return 0;
+  Wav *audio_data =
+      init_wav(PCM_AUDIO_FORMAT, MONO_CHANNEL, CD_SAMPLE_RATE,
+               DEFAULT_SAMPLE_SIZE_IN_BITS, AUDIO_LENGTH_IN_SECONDS);
+
+  uint8_t write_result = write_wav_to_file(audio_data, AUDIO_FILENAME);
+
+  free_wav(audio_data);
+  return write_result;
 }
