32f65a8f9870e08b723274fc948606b2160cc5b9
Thomas Zerr <thomas.zerr@gmail.com>
2026-01-03 18:43:42 +0100

    Add iteration over all mather functions

--- src/main.c
+++ src/main.c
@@ -2,18 +2,28 @@
 #include "../include/tralala.h"
 #include "../include/wav.h"
 #include <stdint.h>
+#include <stdio.h>
 
-#define AUDIO_FILENAME "sample.wav"
+#define MATHERS_COUNT 1
 
 int main() {
-  Wav *audio_data =
-      init_wav(PCM_AUDIO_FORMAT, STEREO_CHANNEL, CD_SAMPLE_RATE,
-               DEFAULT_SAMPLE_SIZE_IN_BITS, AUDIO_LENGTH_IN_SECONDS);
+  char filename_buffer[128];
+  uint8_t (*mathers[1])(uint8_t) = {linear};
+  for (int i = 0; i < MATHERS_COUNT; ++i) {
+    Wav *audio_data =
+        init_wav(PCM_AUDIO_FORMAT, STEREO_CHANNEL, CD_SAMPLE_RATE,
+                 DEFAULT_SAMPLE_SIZE_IN_BITS, AUDIO_LENGTH_IN_SECONDS);
 
-  math_to_tralala(audio_data, linear);
+    math_to_tralala(audio_data, mathers[i]);
 
-  uint8_t write_result = write_wav_to_file(audio_data, AUDIO_FILENAME);
+    snprintf(filename_buffer, sizeof(filename_buffer), "sample_%d.wav", i);
+    uint8_t write_result = write_wav_to_file(audio_data, filename_buffer);
+    free_wav(audio_data);
 
-  free_wav(audio_data);
-  return write_result;
+    if (write_result != 0) {
+      return write_result;
+    }
+  }
+
+  return 0;
 }
